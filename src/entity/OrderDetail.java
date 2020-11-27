6d121e6542c810f567d9f47a9b740f5fe2cd95ce
Dilini Nadeesha <dilininadeeshawellage@gmail.com>
2020-11-28 01:08:50 +0530

    Initial Commit

---
+++ src/entity/OrderDetail.java
@@ -0,0 +1,62 @@
+package entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+public class OrderDetail implements SuperEntity {
+
+    private OrderDetailPK orderDetailPK;
+    private int qty;
+    private BigDecimal unitPrice;
+
+    //no-args Constructor
+    public OrderDetail() {
+    }
+
+    //full-args Constructor
+    public OrderDetail(OrderDetailPK orderDetailPK, int qty, BigDecimal unitPrice) {
+        this.orderDetailPK = orderDetailPK;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    //This Constructor came from DAO Design Pattern
+    public OrderDetail(String orderId, String itemCode, int qty, BigDecimal unitPrice) {
+        this.orderDetailPK = new OrderDetailPK(orderId, itemCode);
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public OrderDetailPK getOrderDetailPK() {
+        return orderDetailPK;
+    }
+
+    public void setOrderDetailPK(OrderDetailPK orderDetailPK) {
+        this.orderDetailPK = orderDetailPK;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDetail{" +
+                "orderDetailPK=" + orderDetailPK +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+}
